e734f188a4d2354c9b98ff6e66f79755c215cc04
Alex Kaplan <259alex952@gmail.com>
2026-02-16 14:01:50 -0800

    Added superstructure, use with all added subsystems

--- frc1388-2026/src/main/java/frc/robot/RobotContainer.java
+++ frc1388-2026/src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,7 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Superstructure;
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
@@ -41,6 +42,8 @@ public class RobotContainer {
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+    public final Superstructure superstructure = new Superstructure(drivetrain);
 
     public RobotContainer() {
         configureBindings();
