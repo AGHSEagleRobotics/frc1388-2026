a76e3ac2f33a4b9630164d9a80dd03dc50dd04b2
Team1388Programming <1388programming@gmail.com>
2026-02-28 14:35:53 -0800

    a

---
+++ frc1388-2026/src/main/java/frc/robot/BuildInfo.java
@@ -0,0 +1,8 @@
+package frc.robot;
+public class BuildInfo {
+  public static final String GIT_VERSION = "e8e46f0";
+  public static final String GIT_BRANCH = "feature/shooterabstractionkicker";
+  public static final String GIT_STATUS = "11 files changed, 133 insertions(+), 24 deletions(-)";
+  public static final String BUILD_DATE = "2026-02-28";
+  public static final String BUILD_TIME = "14:23:13 PST";
+}
