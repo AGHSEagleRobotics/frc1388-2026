31004ee033b8ab857a12aff84a8995289c8cc475
Team1388Programming <1388programming@gmail.com>
2026-03-09 20:31:25 -0700

    quick commit to check dashboard file

--- frc1388-2026/src/main/java/frc/robot/RobotContainer.java
+++ frc1388-2026/src/main/java/frc/robot/RobotContainer.java
@@ -79,6 +79,8 @@ public class RobotContainer {
 
     private final CommandXboxController testJoystick = new CommandXboxController(2);
 
+    private SendableChooser<Command> autoChooser = new SendableChooser<>();
+    
 
     public RobotContainer() {        
         intake = new Intake(new IntakeIOKraken());
@@ -90,31 +92,30 @@ public class RobotContainer {
 
 
         
-         autoChooser = AutoBuilder.buildAutoChooser("Tests");
+        autoChooser = AutoBuilder.buildAutoChooser("Tests");
         SmartDashboard.putData("Auto Mode", autoChooser);
 
         //register commands
 
 
         configureBindings();
-        configureAutonomousCommands();
+        // configureAutonomousCommands();
 
-    //       private void configureAutonomousCommands() {
-    //     Command strikeAStillPose = new InstantCommand(() -> System.out.println("This is kinda boring, no?"));
+        // Command strikeAStillPose = new InstantCommand(() -> System.out.println("This is kinda boring, no?"));
 
-    //      Command moveYourBass = new SequentialCommandGroup(
-    //         new DriveDistanceCommand(drive, .0),   // drive 2 units
-    //         new TurnToAngleCommand(drive, 90)       // turn 90 degrees
-    //     );
-
-
-    //   }
+        //  Command moveYourBass = new SequentialCommandGroup(
+        //     new DriveDistanceCommand(drive, .0),   // drive 2 units
+        //     new TurnToAngleCommand(drive, 90)       // turn 90 degrees
+        // );
 
         NamedCommands.registerCommand("startShooting", superstructure.startShooting());
         NamedCommands.registerCommand("stopShooting", superstructure.stopShooting());
 
+      }
 
-    }
+
+
+    
 
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
