44f2852c94adfcf2b694d5ba1aecb2b6f07d2ccd
Team1388Programming <1388programming@gmail.com>
2026-03-14 14:57:40 -0700

    rps change from 2700 to 2800

--- frc1388-2026/src/main/java/frc/robot/BuildInfo.java
+++ frc1388-2026/src/main/java/frc/robot/BuildInfo.java
@@ -1,8 +1,8 @@
 package frc.robot;
 public class BuildInfo {
-  public static final String GIT_VERSION = "cd5f57b";
+  public static final String GIT_VERSION = "544a427";
   public static final String GIT_BRANCH = "feature/testing2";
-  public static final String GIT_STATUS = "2 files changed, 2 insertions(+), 10 deletions(-)";
+  public static final String GIT_STATUS = "2 files changed, 1 insertion(+), 9 deletions(-)";
   public static final String BUILD_DATE = "2026-03-14";
-  public static final String BUILD_TIME = "14:25:50 PDT";
+  public static final String BUILD_TIME = "14:57:06 PDT";
 }
