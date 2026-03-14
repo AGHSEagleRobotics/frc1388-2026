314645d0c34702a1167c6e4d499c3f3e03b50720
Alejandro Reynoso <alexj.rey23@gmail.com>
2026-03-13 17:53:52 -0700

    Missing commit/push. My bad.

--- frc1388-2026/src/main/java/frc/robot/BuildInfo.java
+++ frc1388-2026/src/main/java/frc/robot/BuildInfo.java
@@ -1,8 +1,8 @@
 package frc.robot;
 public class BuildInfo {
-  public static final String GIT_VERSION = "0e8dbac";
+  public static final String GIT_VERSION = "4db40d7";
   public static final String GIT_BRANCH = "feature/pathplan";
-  public static final String GIT_STATUS = "3 files changed, 29 insertions(+), 8 deletions(-)";
-  public static final String BUILD_DATE = "2026-03-10";
-  public static final String BUILD_TIME = "18:44:23 PDT";
+  public static final String GIT_STATUS = "5 files changed, 63 insertions(+), 85 deletions(-)";
+  public static final String BUILD_DATE = "2026-03-13";
+  public static final String BUILD_TIME = "17:51:33 PDT";
 }
--- frc1388-2026/src/main/java/frc/robot/RobotContainer.java
+++ frc1388-2026/src/main/java/frc/robot/RobotContainer.java
@@ -62,8 +62,10 @@ public class RobotContainer {
     public final ShotCalculator shotcalculator;
     public RobotState robotState;
 
-    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(1.5).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    private double MaxSpeed = 1.0 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top
+                                                                                        // speed
+    private double MaxAngularRate = RotationsPerSecond.of(1.5).in(RadiansPerSecond); // 3/4 of a rotation per second max
+                                                                                     // angular velocity
     private double m_rotationalVelocity = 0;
 
     /* Setting up bindings for necessary control of the swerve drive platform */
@@ -79,10 +81,9 @@ public class RobotContainer {
 
     private final CommandXboxController testJoystick = new CommandXboxController(2);
 
-    private SendableChooser<Command> autoChooser = new SendableChooser<>();
-    
+    private final SendableChooser<Command> autoChooser;
 
-    public RobotContainer() {        
+    public RobotContainer() {
         intake = new Intake(new IntakeIOKraken());
         roller = new Roller(new RollerIOKraken());
         shooter = new Shooter(new ShooterIOKraken());
@@ -90,53 +91,47 @@ public class RobotContainer {
         shotcalculator = new ShotCalculator(drivetrain);
         superstructure = new Superstructure(drivetrain, intake, roller, shooter, hood, shotcalculator);
 
-
-        
-        autoChooser = AutoBuilder.buildAutoChooser("Tests");
-        SmartDashboard.putData("Auto Mode", autoChooser);
-
-        //register commands
-
-
-        configureBindings();
-        configureAutonomousCommands();
+        NamedCommands.registerCommand("setRobotState", superstructure.setRobotState(robotState));
 
         NamedCommands.registerCommand("startShooting", superstructure.startShooting());
         NamedCommands.registerCommand("stopShooting", superstructure.stopShooting());
         NamedCommands.registerCommand("deployIntaking", superstructure.deployIntakingCommand());
-               
+
         NamedCommands.registerCommand("retractIntake", superstructure.retractIntake());
         NamedCommands.registerCommand("shootManually", superstructure.shootManually());
         NamedCommands.registerCommand("setHoodAngleClose", superstructure.setHoodAngleClose());
-                
+
         NamedCommands.registerCommand("setHoodAngleFar", superstructure.setHoodAngleFar());
         NamedCommands.registerCommand("testIntakeDeploy", superstructure.testIntakeDeploy());
         NamedCommands.registerCommand("testIntakeRollers", superstructure.testIntakeRollers());
-        
+
+        NamedCommands.registerCommand("stopIntakeRollers", superstructure.stopIntakeRollers());
         NamedCommands.registerCommand("testRollers", superstructure.testRollers());
+        NamedCommands.registerCommand("stopRollers", superstructure.stopRollers());
+
         NamedCommands.registerCommand("testShooter", superstructure.testShooter());
         NamedCommands.registerCommand("testHood", superstructure.testHood());
-        
         NamedCommands.registerCommand("stopHood", superstructure.stopHood());
-        
-            }
 
-    //       private void configureAutonomousCommands() {
-    //     Command strikeAStillPose = new InstantCommand(() -> System.out.println("This is kinda boring, no?"));
+        configureBindings();
 
-    //      Command moveYourBass = new SequentialCommandGroup(
-    //         new DriveDistanceCommand(drive, .0),   // drive 2 units
-    //         new TurnToAngleCommand(drive, 90)       // turn 90 degrees
-    //     );
+        autoChooser = AutoBuilder.buildAutoChooser("Tests");
+        SmartDashboard.putData("Auto Mode", autoChooser);
 
-
-    //   }
-
-        NamedCommands.registerCommand("startShooting", superstructure.startShooting());
-        NamedCommands.registerCommand("stopShooting", superstructure.stopShooting());
-
+        // register commands
 
     }
+
+    // private void configureAutonomousCommands() {
+    // Command strikeAStillPose = new InstantCommand(() -> System.out.println("This
+    // is kinda boring, no?"));
+
+    // Command moveYourBass = new SequentialCommandGroup(
+    // new DriveDistanceCommand(drive, .0), // drive 2 units
+    // new TurnToAngleCommand(drive, 90) // turn 90 degrees
+    // );
+
+    // }
 
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
@@ -144,31 +139,25 @@ public class RobotContainer {
         double leftX = MathUtil.applyDeadband(joystick.getLeftY(), 0.1);
         double leftY = MathUtil.applyDeadband(joystick.getLeftX(), 0.1);
 
-        
         double xVelocity = -MaxSpeed * scale(leftX, 2.5);
         double yVelocity = -MaxSpeed * scale(leftY, 2.5);
-        
 
         drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(xVelocity) // Drive forward with negative Y (forward)
-                    .withVelocityY(yVelocity) // Drive left with negative X (left)
-                    .withRotationalRate(getRotationalVelocity()) // Drive counterclockwise with negative X (left)
-            )
-        );
+                // Drivetrain will execute this command periodically
+                drivetrain.applyRequest(() -> drive.withVelocityX(xVelocity) // Drive forward with negative Y (forward)
+                        .withVelocityY(yVelocity) // Drive left with negative X (left)
+                        .withRotationalRate(getRotationalVelocity()) // Drive counterclockwise with negative X (left)
+                ));
 
         // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
         final var idle = new SwerveRequest.Idle();
         RobotModeTriggers.disabled().whileTrue(
-            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
-        );
+                drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
         joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        joystick.b().whileTrue(drivetrain.applyRequest(
+                () -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
         joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
@@ -185,7 +174,7 @@ public class RobotContainer {
 
         // sets robot shoot on/off while right trigger is held
         if (superstructure.pointedAtTarget()) {
-        joystick.rightTrigger().whileTrue(superstructure.startShooting());
+            joystick.rightTrigger().whileTrue(superstructure.startShooting());
         }
         joystick.rightTrigger().onFalse(superstructure.stopShooting());
 
@@ -203,7 +192,7 @@ public class RobotContainer {
         joystick.y().onTrue(superstructure.setHoodAngleFar());
 
         // TESTING JOYSTICK
-        
+
         // intake deploy and retract
         testJoystick.leftBumper().onTrue(superstructure.testIntakeDeploy());
         testJoystick.leftTrigger().onTrue(superstructure.retractIntake());
@@ -232,33 +221,11 @@ public class RobotContainer {
         testJoystick.pov(270).whileTrue(hood.sysIdDynamicCommand(Direction.kReverse));
     }
 
-    
-
     public Command getAutonomousCommand() {
-    //     System.out.println("before running");
-    //     System.out.println("*");
-    //     System.out.println("*");
-    //     System.out.println("*");
-    //     System.out.println("*");
-    //     System.out.println("*");
-    //     System.out.println("*");
-    //     System.out.println("*");
-    //     try{
-    //         System.out.println("post try");
-    //         // Load the path you want to follow using its name in the GUI
-    //         PathPlannerPath testPath = PathPlannerPath.fromPathFile("Lachemann testpath");
-            
-    //         // Create a path following command using AutoBuilder. This will also trigger event markers.
-    //         System.out.println("returning testpath");
-    //         return AutoBuilder.followPath(testPath);
-            
-    //     } catch (Exception e) {
-    //         DriverStation.reportError("Big oops: " + e.getMessage(), e.getStackTrace());
-    //         return Commands.none();
-    // }
-    return autoChooser.getSelected();
+        
 
-         
+        return autoChooser.getSelected();
+
     }
 
     public double getRotationalVelocity() {
