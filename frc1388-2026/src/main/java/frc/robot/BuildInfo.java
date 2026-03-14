0c3f41000469ae38d6c039a517f35602ef14ebac
Team1388Programming <1388programming@gmail.com>
2026-03-14 11:11:44 -0700

    fix

--- frc1388-2026/src/main/java/frc/robot/BuildInfo.java
+++ frc1388-2026/src/main/java/frc/robot/BuildInfo.java
@@ -1,8 +1,8 @@
 package frc.robot;
 public class BuildInfo {
-  public static final String GIT_VERSION = "3c70883";
+  public static final String GIT_VERSION = "a291585";
   public static final String GIT_BRANCH = "feature/testing2";
-  public static final String GIT_STATUS = "3 files changed, 6 insertions(+), 9 deletions(-)";
+  public static final String GIT_STATUS = "1 file changed, 8 deletions(-)";
   public static final String BUILD_DATE = "2026-03-14";
-  public static final String BUILD_TIME = "08:16:18 PDT";
+  public static final String BUILD_TIME = "11:05:02 PDT";
 }
