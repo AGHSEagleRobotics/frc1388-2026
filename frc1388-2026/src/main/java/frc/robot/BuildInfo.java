3214e118a4e84cb788b8ddf0fca3e21790b877db
Diego Arevalo <1388programming@gmail.com>
2026-03-15 09:47:43 -0700

    kicker-shooter ratio constant changed from 3/3 to 4/3

--- frc1388-2026/src/main/java/frc/robot/BuildInfo.java
+++ frc1388-2026/src/main/java/frc/robot/BuildInfo.java
@@ -1,8 +1,8 @@
 package frc.robot;
 public class BuildInfo {
-  public static final String GIT_VERSION = "31d4e75";
+  public static final String GIT_VERSION = "aa9953d";
   public static final String GIT_BRANCH = "dev";
-  public static final String GIT_STATUS = "3 files changed, 6 insertions(+), 16 deletions(-)";
+  public static final String GIT_STATUS = "2 files changed, 1 insertion(+), 9 deletions(-)";
   public static final String BUILD_DATE = "2026-03-15";
-  public static final String BUILD_TIME = "09:14:42 PDT";
+  public static final String BUILD_TIME = "09:47:07 PDT";
 }
--- frc1388-2026/src/main/java/frc/robot/Constants.java
+++ frc1388-2026/src/main/java/frc/robot/Constants.java
@@ -87,7 +87,7 @@ public final class Constants {
 
       public static final double MANUAL_SHOOT_FAR = 3000.0/60.0;
 
-      public static final double KICKER_TO_SHOOTER_RATIO = 3.0/3.0;
+      public static final double KICKER_TO_SHOOTER_RATIO = 4.0/3.0;
 
       public static final Transform3d BALL_TRANSFORM_CENTER = new Transform3d(0, 0, 0, Rotation3d.kZero);
       public static final InterpolatingDoubleTreeMap DISTANCE_TO_SHOT_RPM = new InterpolatingDoubleTreeMap();
