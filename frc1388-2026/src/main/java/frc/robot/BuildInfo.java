c830f67c24b21a437fa8283c133520f6669edf85
Alex Kaplan <259alex952@gmail.com>
2026-04-08 21:49:15 -0700

    updated code

--- frc1388-2026/src/main/java/frc/robot/BuildInfo.java
+++ frc1388-2026/src/main/java/frc/robot/BuildInfo.java
@@ -1,8 +1,8 @@
 package frc.robot;
 public class BuildInfo {
-  public static final String GIT_VERSION = "eda1f23";
-  public static final String GIT_BRANCH = "feature/competitiondashboard";
-  public static final String GIT_STATUS = "6 files changed, 12 insertions(+), 16 deletions(-)";
-  public static final String BUILD_DATE = "2026-04-07";
-  public static final String BUILD_TIME = "19:35:14 PDT";
+  public static final String GIT_VERSION = "419c99d";
+  public static final String GIT_BRANCH = "dev";
+  public static final String GIT_STATUS = "3 files changed, 12 insertions(+), 20 deletions(-)";
+  public static final String BUILD_DATE = "2026-04-08";
+  public static final String BUILD_TIME = "21:48:19 PDT";
 }
