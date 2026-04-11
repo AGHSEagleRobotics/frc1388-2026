538d470ac42d755bfd0e7350644c430baad9f673
Alex Kaplan <259alex952@gmail.com>
2026-04-11 08:35:38 -0700

    New autos

--- frc1388-2026/src/main/java/frc/robot/BuildInfo.java
+++ frc1388-2026/src/main/java/frc/robot/BuildInfo.java
@@ -1,8 +1,8 @@
 package frc.robot;
 public class BuildInfo {
-  public static final String GIT_VERSION = "80797b0";
+  public static final String GIT_VERSION = "66e8e3c";
   public static final String GIT_BRANCH = "dev";
   public static final String GIT_STATUS = "1 file changed, 8 deletions(-)";
   public static final String BUILD_DATE = "2026-04-10";
-  public static final String BUILD_TIME = "17:15:26 PDT";
+  public static final String BUILD_TIME = "17:16:58 PDT";
 }
